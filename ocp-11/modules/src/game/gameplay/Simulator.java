393f58ecfab76cb1e5cb342caca0322f7ca4076d
Tiago Luna <tiagodeluna@hotmail.com>
2021-06-06 17:29:24 +0200

    Created Java OCP-11 project

---
+++ ocp-11/modules/src/game/gameplay/Simulator.java
@@ -0,0 +1,7 @@
+package game.gameplay;
+
+public class Simulator {
+    public static void main(String... args) {
+        System.out.println("Nothing happens...");
+    }
+}
---
+++ ocp-11/src/zoo/animal/feeding/Task.java
@@ -0,0 +1,4 @@
+package zoo.animal.feeding;
+
+public class Task {
+}
